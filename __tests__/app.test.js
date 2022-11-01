a18b663ed2eb4c963c74be91449175f063bceec2
Tanner Richards <Tanner.richards990@gmail.com>
2022-11-01 12:18:32 -0700

    built out index html

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,1 @@
-const request = require('supertest');
-const app = require('../lib/app');
-
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
-  });
-});
+const setup = require('../data/setup');
